007d9050fceee4edbd59d988827343e431be66ac
Batman <batman@Mac-mini.local>
2020-06-15 16:07:24 +0530

    SEO Complete. Posts Pending

--- wp-content/themes/foodanddesserts/header.php
+++ wp-content/themes/foodanddesserts/header.php
@@ -14,6 +14,15 @@
 <html class="no-js" <?php language_attributes(); ?>>
 
 	<head>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168154315-1"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-168154315-1');
+		</script>
 
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
